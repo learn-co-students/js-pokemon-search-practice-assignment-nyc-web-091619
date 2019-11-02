82a67fc95ab4133a464aba9aad8d140f44fea39e
Altaf Quadri <altafmquadri@gmail.com>
2019-11-02 18:03:34 -0400

    fixed the functionality of search function in the search bar, had to make sure to use Array.from when iterating through a collection

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ const api = 'http://localhost:3000/pokemon';
 
 document.addEventListener('DOMContentLoaded', () => {
   //console.log(POKEMON)
+  POKEMON
   
   fetchPokemon();
+  filterPokemon(POKEMON)
   
 })
 
@@ -15,13 +17,21 @@ let fetchPokemon = function(){
       return response.json();
     })
     .then(function(pokemons){
-      iteratePokemon(pokemons)
-       return filterPokemon(pokemons)
+      return iteratePokemon(pokemons)
+       //return filterPokemon(pokemons)
     }) 
+    //
 }
 
 //iterate through
 function iteratePokemon(pokemons) {
+  // let pkc = document.getElementsByClassName("pokemon-card")
+  // console.log(`${pkc.length}, I am pkc length`)
+  // for (let i = 0; i < pkc.length; i++) {
+  //   console.log(i)
+  //   pkc[i].remove()
+  // }
+  
   pokemons.forEach(function(pokemon){
     return appendPokemon(pokemon)
   })
@@ -29,7 +39,7 @@ function iteratePokemon(pokemons) {
 
 //add pokemon onto the page
 function appendPokemon(pokemon) {
-  //
+  // console.log("appending")
   center = document.querySelector("center")
   center.style.display = "none"
 
@@ -64,23 +74,65 @@ function appendPokemon(pokemon) {
 
   let pkc = document.getElementById("pokemon-container")
   pkc.appendChild(pokemonCardContainer)
+  // console.log(`i am adding ${pokemon}`)
 }
 
+let pokemonCardClass = document.getElementsByClassName("pokemon-card")
 
-let filterPokemon = function(pokemons) {
+let filterPokemon = function(pokemon) {
   let searchBar = document.getElementById("pokemon-search-input")
-  searchBar.addEventListener('input', function(e){
-    console.log("i am searching, outside from the filter")
-    console.dir(e.target.value)
-    filteredPokemnon = pokemons.filter(function(pokemon) {
-      if (pokemon.name.includes(e.target.value)) {
-        console.log("i am in the filter function")
-        console.dir(e.target.value)
-        return appendPokemon(pokemon)
-        debugger
-      }
+  
+  console.log(`${pokemonCardClass.length}, I am pokemon-card length at filterPokemon`)
+  
+  searchBar.addEventListener('input', function(e) {
+    
+    console.log(`${pokemonCardClass.length}, I am pokemon-card length, after the event listener is called`)
+
+    /* Hi I am a current mod 3 student: 
+    
+    Can someone explain the difference between these two, 
+    I would expect it to have the same behavior, but results of first
+    one, do not produce the correct values, whenreas the other one
+    does. */
+    
+    // this one does not work
+    // HTMLCollection.prototype.forEach = Array.prototype.forEach
+    // pokemonCardClass.forEach(function(element){
+    //   element.remove()
+    // })
+
+    //This one works
+    Array.from(pokemonCardClass).forEach(function(element) {
+      element.remove()
     })
+
+    
+
+    // for (const element of pokemonCardClass) {
+    //   console.log(`The pokemonCardClass length is ${pokemonCardClass.length}`)
+    //   element.remove()
+    // }
+
+  //   for (let i = 0; i < fixedPkcLenght; i++) {
+  //   console.log(`The pkc length is ${pkc.length}, and i has iterated ${i} times`)
+  //   pkc[i].remove()
+  // }
+    console.log("In filtered pokemon, after the event clicker")
+    
+    filteredPokemon = pokemon.filter(function(pokemon) {
+
+      if (pokemon.name.includes(e.target.value)) {
+        
+        console.log(e.target.value)
+        
+        return pokemon
+      } 
+    })
+    
+    iteratePokemon(filteredPokemon)
+
+    // if(!!filteredPokemon.length) {
+    //   center.style.display = "block"
+    // }
   })
-
-
 }
